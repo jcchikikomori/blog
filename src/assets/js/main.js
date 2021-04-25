c638c4187357d9cca583883c853ba5ef2ee26386
John Cyrill Corsanes <jccorsanes@protonmail.com>
2021-04-25 19:19:56 +0800

    refactor(eslint, layout, about, js, scss, loading): add loading & document handling using jquery

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
-  console.log('Hello World');
+  var pretitle = "JCC Blog - ";
+  var title = $("#title").text() || "Articles";
+  var __path = window.location.pathname;
+
+  if (__path !== "/") {
+    $(document).prop("title", pretitle + title);
+  }
+
+  $(".loading").hide();
 
   // Check for click events on the navbar burger icon
   $(".navbar-burger").click(function() {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
   });
 });
