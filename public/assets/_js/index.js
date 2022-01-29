976f39ee4bcb7aa7af4de9e5e1bb37ca5e79a470
John Cyrill Corsanes <johncyril@gorated.ph>
2022-01-29 20:38:23 +0800

    dependencies cleanup. moving packages to node_modules over hardcoded one

--- public/assets/_js/index.js
+++ public/assets/_js/index.js
@@ -1,16 +1,23 @@
+// BulmaJS
+require("bulmajs")
+
 // DayJS
 const dayjs = require("dayjs")
 
+// JQuery
+import $  from "jquery"
+window.$ = window.jQuery = $
+
 // Google Analytics
+// TODO: Use a different bundle
 window.dataLayer = window.dataLayer || []
 function gtag(){dataLayer.push(arguments)}
 gtag('js', new Date())
 gtag('config', 'UA-53297367-3')
 
-// TODO: Replace JQuery as the world abadons this library, year by year
-$(document).ready(function() {
+$(function() {
   // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
+  $(".navbar-burger").on("click", function() {
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
     $(".navbar-burger").toggleClass("is-active")
     $(".navbar-menu").toggleClass("is-active")
